af3cf85fbc78d48faac32e10ccf9ef33f374830f
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-08-25 19:15:56 +0200

    Added desing-patterns-1 task

---
+++ design-patterns-1/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * [design-patterns-1] Sequence
+ */
+
+class Solution {
+  public static void main(String[] args) {
+    
+  }
+}
+
+
+class Subject {
+  
+}
+
+class AnObject {
+  
+}
