903cdb95cebbc697b82f43dc4c27e0448a543bf5
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-08-27 21:55:22 +0200

    Extended the implementation

--- design-patterns-1/Solution.java
+++ design-patterns-1/Solution.java
@@ -8,15 +8,46 @@ import java.util.*;
 
 class Solution {
   public static void main(String[] args) {
+    Subject subject = new Subject();
     
+    subject.subscribe(new AnObject());
+    subject.subscribe(new AnotherObject());
+    
+    subject.dispatch();
   }
 }
 
 
 class Subject {
   
+  public void subscribe(Observator observator) {
+    // TODO
+  }
+  
+  public void dispatch() {
+    // TODO
+  }
+  
 }
 
-class AnObject {
+class AnObject implements Observator {
+  
+  public void update() {
+    System.out.println("It's AnObject");
+  }
+  
+}
+
+class AnotherObject implements Observator {
+   
+  public void update() {
+    System.out.println("It's AnotherObject");
+  }
+  
+}
+
+interface Observator {
+  
+  public void update();
   
 }
